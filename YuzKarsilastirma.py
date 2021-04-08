8cf85964e6bb593f3b8278774e48a0c40d396758
Süleyman Kaya <71667913+SuleymanKaya1@users.noreply.github.com>
2021-04-08 14:51:17 +0300

    Update YuzKarsilastirma.py

--- YuzKarsilastirma.py
+++ YuzKarsilastirma.py
@@ -1,24 +1,27 @@
 import cv2
 import face_recognition
- 
-imgSuleyman = face_recognition.load_image_file('YuzKarsilastirmaKlasoru/Suleyman Kaya.jpg')
-imgSuleyman = cv2.cvtColor(imgSuleyman, cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('YuzKarsilastirmaKlasoru/Suleyman Kaya 2.jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
- 
-faceLoc = face_recognition.face_locations(imgSuleyman)[0]
-encodeElon = face_recognition.face_encodings(imgSuleyman)[0]
-cv2.rectangle(imgSuleyman, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
- 
-faceLocTest = face_recognition.face_locations(imgTest)[0]
-encodeTest = face_recognition.face_encodings(imgTest)[0]
-cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
- 
-results = face_recognition.compare_faces([encodeElon],encodeTest)
-faceDis = face_recognition.face_distance([encodeElon],encodeTest)
-print(results,faceDis)
-cv2.putText(imgTest,f'{results} {round(faceDis[0],2)}',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
- 
-cv2.imshow('Fotograf 1', imgSuleyman)
-cv2.imshow('Fotograf 2',imgTest)
+
+foto1path = input("Fotografiniz cihazinizda nerede bulunuyor: ")
+foto2path = input("Karsilastirmak istediginiz fotograf cihazinizda nerede bulunuyor: ")
+
+fotograf1 = face_recognition.load_image_file(foto1path)
+fotograf1 = cv2.cvtColor(fotograf1, cv2.COLOR_BGR2RGB)
+fotograf2 = face_recognition.load_image_file(foto2path)
+fotograf2 = cv2.cvtColor(fotograf2, cv2.COLOR_BGR2RGB)
+
+YuzLokasyonu = face_recognition.face_locations(fotograf1)[0]
+encodeImg1 = face_recognition.face_encodings(fotograf1)[0]
+cv2.rectangle(fotograf1, (YuzLokasyonu[3], YuzLokasyonu[0]), (YuzLokasyonu[1], YuzLokasyonu[2]), (255, 0, 255), 2)
+
+yuzLokasyonTest = face_recognition.face_locations(fotograf2)[0]
+encodeImg2 = face_recognition.face_encodings(fotograf2)[0]
+cv2.rectangle(fotograf2, (yuzLokasyonTest[3], yuzLokasyonTest[0]), (yuzLokasyonTest[1], yuzLokasyonTest[2]), (255, 0, 255), 2)
+
+sonuclar = face_recognition.compare_faces([encodeImg1], encodeImg2)
+faceDis = face_recognition.face_distance([encodeImg1], encodeImg2)
+print(sonuclar, faceDis)
+cv2.putText(fotograf2, f'{sonuclar} {round(faceDis[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+
+cv2.imshow('Fotograf 1', fotograf1)
+cv2.imshow('Fotograf 2', fotograf2)
 cv2.waitKey(0)
